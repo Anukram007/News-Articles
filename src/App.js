0c702b171fd4a38651d8eeb3564aaf747ddca428
Anukram <anukram.mukherjee@cbnits.com>
2021-09-14 14:03:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { Component } from 'react'
+import { Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      NewsData: []
+    }
+  }
+
+  componentDidMount = () => {
+    axios.get("https://recengine.intoday.in/recengine/at/getarticles")
+      .then(async res => (
+        await this.setState({
+          NewsData: res.data.data
+        })
+      ))
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.NewsData.map((value, index) => (
+          <span>
+            <th>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={value.mobile_image} />
+                <Card.Body>
+                  <Card.Title>{value.title}</Card.Title>
+                </Card.Body>
+              </Card>
+            </th>
+          </span>
+        ))
+        }
+      </>
+    )
+  }
 }
-
-export default App;
